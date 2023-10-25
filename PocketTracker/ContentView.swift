8ee2c7dc2753ffb84dd99393cfb3f07cefbe62f5
William <will_cuellard@Hotmail.com>
2023-10-25 12:20:51 -0600

    Initial Commit

---
+++ PocketTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PocketTracker
+//
+//  Created by macbook pro on 25/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PocketTracker/PocketTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketTrackerApp.swift
+//  PocketTracker
+//
+//  Created by macbook pro on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PocketTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
